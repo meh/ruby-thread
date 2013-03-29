2dad78de2e0b8920a51737c8a8ab5cd6aecd5fd5
meh <meh@schizofreni.co>
2013-03-29 01:55:38 +0100

    tests: test for pipe emptyness

--- tests/pipe_spec.rb
+++ tests/pipe_spec.rb
@@ -12,4 +12,14 @@ describe Thread::Pipe do
 		p.deq.should == 16
 		p.deq.should == 32
 	end
+
+	it 'empty works properly' do
+		p = Thread |-> d { sleep 0.2; d * 2 } |-> d { d * 4 }
+
+		p.empty?.should == true
+		p.enq 42
+		p.empty?.should == false
+		p.deq
+		p.empty?.should == true
+	end
 end
