75fb782c2fa9f744f6f3bdb1aa29dd09dabb39eb
meh <meh@schizofreni.co>
2013-05-15 19:52:47 +0200

    tests: removing old tests

--- tests/every_spec.rb
+++ tests/every_spec.rb
@@ -4,21 +4,8 @@ require 'thread/every'
 
 describe Thread::Every do
 	it 'delivers a value properly' do
-		e = Thread.every(5) { 42 }
+		e = Thread.every(5) { sleep 0.5; 42 }
 
 		e.value.should == 42
-	end
-
-	it 'sees it as old properly' do
-		e = Thread.every(0.8) { 42 }
-
-		e.value.should == 42
-		e.old?.should  == true
-
-		sleep 1
-
-		e.old?.should == false
-		e.value.should == 42
-		e.old?.should == true
 	end
 end
